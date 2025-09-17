ec1c1fc79c56cb59b7b98a5372b083419c795446
Mauro Triveño <mtr0338@proven.local>
2025-09-17 17:44:10 +0200

    Adding changes a my_file

--- my_file.java
+++ my_file.java
@@ -1,7 +1,8 @@
 public Class my_file{
 
-	public staic void main(String args[]){
+	public staic void main (String args[]){
 		Sysetm.out.println("First project Git");
 		System.out.println("New line for my code");
+		System.out.println("Adding new line");	
 	}
 }
