218dedc6d6f6607d83ba5ce58f8a10ee8e76b647
Mauro Triveño <mtr0338@proven.local>
2025-09-17 16:27:52 +0200

    Minor changes on my_file and adding my_second_file

--- my_file.java
+++ my_file.java
@@ -2,5 +2,6 @@ public Class my_file{
 
 	public staic void main(String args[]){
 		Sysetm.out.println("First project Git");
+		System.out.println("New line for my code");
 	}
 }
---
+++ my_second_file.java
@@ -0,0 +1,3 @@
+public class my_second_file{
+
+}
